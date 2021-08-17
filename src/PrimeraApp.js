b0f01265f7f813f58519ef4217b032db0489237a
Fabian Muller <fabian.muller.saavedra@gmail.com>
2021-08-17 07:51:18 -0400

    se agrega el primer componente en react

---
+++ src/PrimeraApp.js
@@ -0,0 +1,26 @@
+// FC
+import React from 'react';
+import PropTypes from 'prop-types'
+
+const PrimeraApp = ( { saludo, subtitulo } ) => {
+
+    
+
+    return (
+        <>
+            <h1> { saludo } </h1>
+            {/* <pre> { JSON.stringify( saludo, null, 3 ) } </pre> */}
+            <p> { subtitulo }</p>
+        </>
+    )
+}
+
+PrimeraApp.propTypes = {
+    saludo : PropTypes.string.isRequired,
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un substitulo'
+}
+
+export default PrimeraApp;
